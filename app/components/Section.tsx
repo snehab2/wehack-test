f8262fe426a4d2611ba34c95d779b6d3e86c0ed0
Sneha <snehab@prodigy.net>
2024-05-04 23:30:25 -0500

    finished jumbotron

---
+++ app/components/Section.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+function Section() {
+  return (
+    // any property that doesn't start with sm:, md:, lg:, etc are default values for mobile
+    // as you increase the screen size, you need to adjust the following for different screen sizes:
+    // flex-direction (do the opposite of mobile's flex direction)
+    // width for divs (decrease width size as screen size increases)
+    // height for divs (increase height size as screen size increases)
+    // image size
+    // font size
+    
+    // general formatting for className
+    // element name, width (breakpoints), height (breakpoints), flex, flex-wrap, flex-direction (breakpoints), items-center, justify-content, gap, margin, padding
+
+    <section className='parent-container w-screen h-screen flex flex-wrap flex-column sm:flex-row md:flex-row lg:flex-row items-center justify-center gap-y-4 m-0 p-10'>
+        <div className='child-container w-full sm:w-1/2 md:w-1/2 lg:w-1/2 h-1/2 sm:h-full md:h-full lg:h-full flex flex-wrap flex-row sm:flex-column md:flex-column lg:flex-column items-center justify-center'>
+            <img className="h-auto max-w-full sm:max-w-sm md:max-w-md lg:max-w-lg" src="/LogoSparkle.png" alt="wehack logo"></img>
+        </div>
+        <div className='child-container w-full sm:w-1/2 md:w-1/2 lg:w-1/2 h-1/2 sm:h-full md:h-full lg:h-full flex flex-wrap flex-row sm:flex-column md:flex-column lg:flex-column items-center justify-center'>
+            <div className='child-container flex flex-wrap flex-column gap-y-7'>
+                <div className='child-container w-full flex flex-wrap flex-row items-center justify-center'>
+                    <h1 className='jumbo-text font-medium text-5xl lg:text-8xl text-center'>WEHack 2024</h1>
+                </div>
+                <div className='child-container w-full flex flex-wrap flex-row items-center justify-center'>
+                    <h2 className='jumbo-text text-3xl lg:text-5xl text-center'>Enchanted to be Here</h2>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Section
---
+++ app/sections/Jumbotron.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Section from '../components/Section'
+import './Jumbotron.css'
+
+function Jumbotron() {
+  return (
+    <Section/>
+  )
+}
+
+export default Jumbotron
