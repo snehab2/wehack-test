6a82dd0fc4ceafaaa04dd4e8641061df371f92a7
Sneha <snehab@prodigy.net>
2024-05-11 18:20:19 -0500

    added stats section

---
+++ app/components/Stats/Stats.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import './Stats.css'
+function Stats() {
+  return (
+    // stats-container flex flex-wrap flex-col gap-y-20 justify-between sm:flex-row md:flex-row: lg:flex-row pt-40 sm:pt-40 md:pt-48 lg:pt-72
+
+        <div className='stats-container flex flex-col gap-y-20 lg:flex-row pt-40 sm:pt-40 md:pt-40 lg:pt-28 lg:space-x-12'>
+            <div className='statsOne flex flex-wrap flex-col align-center justify-between gap-y-3'>
+                <img className="wehack-mascot h-auto max-w-60 sm:max-w-64 md:max-w-72 lg:max-w-72" src="./2024 images/IMG_0843(2).png" alt="wehack mascot"></img>
+                <div className='stats-text flex flex-col gap-y-3'>
+                    <h1 className='stats-title text-center text-[#FFE9D7] text-5xl md:text-5xl lg:text-5xl'>280+</h1>
+                    <p className='text-center text-[#FFE9D7] font-medium text-lg lg:text-xl'>Hackers</p>
+                </div>
+            </div>
+            <div className='statsTwo flex flex-wrap flex-col align-center justify-between gap-y-3'>
+                <img className="wehack-mascot h-auto max-w-60 sm:max-w-64 md:max-w-72 lg:max-w-72 lg:mt-20" src="./2024 images/IMG_0845(2).png" alt="wehack mascot"></img>
+                <div className='stats-text flex flex-col gap-y-3'>
+                    <h1 className='stats-title text-center text-[#FFE9D7] text-5xl md:text-5xl lg:text-5xl'>50+</h1>
+                    <p className='text-center text-[#FFE9D7] font-medium text-lg lg:text-xl'>Projects Submitted</p>
+                </div>
+            </div>
+            <div className='statsThree flex flex-wrap flex-col align-center justify-between gap-y-3'>
+            {/* IMG_0846(1) */}
+                <img className="wehack-mascot h-auto max-w-60 sm:max-w-64 md:max-w-72 lg:max-w-72 lg:mt-20" src="./2024 images/IMG_0846(1).png" alt="wehack mascot"></img>
+                <div className='stats-text flex flex-col gap-y-3'>
+                    <h1 className='stats-title text-center text-[#FFE9D7] text-5xl md:text-5xl lg:text-5xl'>48</h1>
+                    <p className='text-center text-[#FFE9D7] font-medium text-lg lg:text-xl'>Workshops and Mini-Events</p>
+                </div>
+            </div>
+        </div>
+        
+
+
+  )
+}
+
+export default Stats
--- app/sections/AboutPage/About.tsx
+++ app/sections/AboutPage/About.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import './About.css'
-
+import Stats from '@/app/components/Stats/Stats'
 function About() {
   return (
     <section className='parent-container about-section bg-[#755642]' id="about-wehack">
       {/* group 1 */}
-      <div className='about-group flex flex-wrap flex-column sm:flex-row md:flex-row: lg:flex-row'>
+      <div className='about-group flex flex-wrap flex-col sm:flex-row md:flex-row: lg:flex-row'>
         <div className='child-container'>
-            <div className='paragraph-1 flex flex-wrap flex-column gap-y-5'>
+            <div className='paragraph-1 flex flex-wrap flex-col gap-y-5'>
                 <h1 className='about-title text-[#FFE9D7] font-medium text-4xl lg:text-5xl text-left'>Dallas' Largest Inclusive Hackathon</h1>
                 <p className='about-text text-[#FFE9D7] font-medium text-lg lg:text-xl text-left'>UTD's women-driven tech organizations are excited to join forces and present our 24-hour hackathon uniting women and non-binary students. Our goal is to create an inclusive environment that highlights and empowers women, femme, and non-binary students through technology.</p>
             </div>
@@ -18,16 +18,19 @@ function About() {
       </div>
 
       {/* group 2 */}
-      <div className='about-group flex flex-wrap-reverse flex-column sm:flex-row md:flex-row: lg:flex-row pt-32 sm:pt-36 md:pt-48 lg:pt-72'>
+      <div className='about-group flex flex-wrap-reverse flex-col sm:flex-row md:flex-row: lg:flex-row pt-32 sm:pt-36 md:pt-48 lg:pt-72'>
         <div className='child-container'>
             <img className="about-image h-auto max-w-76 sm:max-w-72 md:max-w-96 lg:max-w-full" src="./2024 images/IMG_1752.jpg" alt="wehack logo"></img>
         </div>
         <div className='child-container'>
-            <div className='paragraph-2 flex flex-wrap flex-column gap-y-5'>
+            <div className='paragraph-2 flex flex-wrap flex-col gap-y-5'>
                 <p className='about-text text-[#FFE9D7] font-medium text-lg lg:text-xl text-left'>We aim to encourage more underrepresented individuals to harness their skills by creating software or hardware projects of their own. Whether it's networking with sponsors and hackers, gaining development and workshop experience, or competing against other teams for challenges and prizes, WEHack offers participants an opportunity to end the weekend with unique takeaways.</p>
             </div>
         </div>
       </div>
+
+      {/* stats */}
+      <Stats/>
     </section>
   )
 }
