a307fe56f550429222c78911611b213304feef83
Sneha <snehab@prodigy.net>
2024-05-11 20:09:59 -0500

    updated footer formatting

--- app/components/Button/Button.tsx
+++ app/components/Button/Button.tsx
@@ -3,9 +3,9 @@ import './Button.css'
 
 function Button({buttonText, link} : {buttonText : String, link : any}) {
   return (
-        <a href={link} className="text-xl">
-          <button className="primary-btn bg-[#909634] text-[#FFE9D7] w-full py-3 px-5">{buttonText}</button>
-        </a>
+      <a href={link} className="text-xl" target="_blank">
+        <button className="primary-btn bg-[#909634] text-[#FFE9D7] w-full py-3 px-5">{buttonText}</button>
+      </a>
   )
 }
 
--- app/sections/Footer/Footer.tsx
+++ app/sections/Footer/Footer.tsx
@@ -4,12 +4,12 @@ import Button from '@/app/components/Button/Button'
 
 function Footer() {
   return (
-    <div className='footer-container w-full h-auto flex flex-col items-center justify-center p-20 gap-y-10'>
-        <div className='main-container w-full flex flex-wrap flex-col items-center content-center gap-y-6'>
+    <div className='footer-container w-full h-auto flex flex-col md:flex-row lg:flex-row items-center justify-center md:items-start md:justify-start lg:items-start lg:justify-start p-20 gap-y-10 md:gap-x-3 lg:gap-x-5'>
+        <div className='main-container w-full flex flex-wrap flex-col items-center content-center gap-y-6 md:items-start md:content-start lg:items-start lg:content-start'>
             <img className="wehack-logo h-auto max-w-60 sm:max-w-64 md:max-w-72 lg:max-w-72" src="./2024 images/LogoSparkle.png" alt="wehack logo"></img>
             <p className='about-text text-[#FFE9D7] font-medium text-xl lg:text-xl text-center'>Made with 💪🤍 by WEHack 2025</p>
         </div>
-        <div className='navigation-container w-full flex flex-wrap flex-col content-center justify-center gap-y-6'>
+        <div className='navigation-container w-full flex flex-wrap flex-col content-center justify-center gap-y-6 md:w-1/2 lg:w-1/2'>
             <h1 className='about-title text-[#FFE9D7] font-medium text-3xl lg:text-4xl text-center'>Navigation</h1>
             {/* <a href="#home">Home</a>
             <a href="#about-wehack">About</a>
@@ -18,7 +18,7 @@ function Footer() {
             <Button link={'#about-wehack'} buttonText={'About'}/>
             <Button link={'https://static.mlh.io/docs/mlh-code-of-conduct.pdf'} buttonText={'MLH Conduct'}/>
         </div>
-        <div className='contact-us-container w-full flex flex-wrap flex-col content-center justify-center gap-y-6'>
+        <div className='contact-us-container w-full flex flex-wrap flex-col content-center justify-center gap-y-6 md:w-1/2 lg:w-1/2'>
             <h1 className='about-title text-[#FFE9D7] font-medium text-3xl lg:text-4xl text-left'>Contact Us</h1>
             {/* <a href="mailto:wehackutd@gmail.com" target="_blank">Email</a>
             <a href="https://www.instagram.com/wehackutd/" target="_blank">Instagram</a>
