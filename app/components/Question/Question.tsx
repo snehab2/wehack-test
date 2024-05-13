90a3ac64685b8de31a2a5ac6fd515d2f6ce61887
Sneha <snehab@prodigy.net>
2024-05-12 21:29:12 -0500

    updated text size for FAQ answer

--- app/components/Question/Question.tsx
+++ app/components/Question/Question.tsx
@@ -5,7 +5,7 @@ function Question({question, answer} : {question : String, answer : String}) {
     <div className="question w-full lg:w-10/12">
         <details>
             <summary className='flex flex-wrap justify-between list-none w-full bg-[#FFE9D7] text-[#4A3628] p-2 text-xs md:text-base lg:text-base'>{question}</summary>
-                <div className="panel text-sm">
+                <div className="panel text-sm lg:text-base">
                     {answer}
                 </div>
         </details>
